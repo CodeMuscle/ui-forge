a5995b84ef8f98b3e64ad8a0067dcb7ebd6e9603
CodeMuscle <investportal99@gmail.com>
2024-08-12 08:53:48 +0530

    /sell page

--- app/sell/page.tsx
+++ app/sell/page.tsx
@@ -1,42 +1,42 @@
-"use client";
+'use client'
 
-import { SellProduct, type State } from "@/app/actions";
+import { SellProduct, type State } from '@/app/actions'
 import {
   Card,
   CardContent,
   CardDescription,
   CardFooter,
   CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { type JSONContent } from "@tiptap/react";
+  CardTitle
+} from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { type JSONContent } from '@tiptap/react'
 // import { redirect } from "next/navigation";
-import { useEffect, useState } from "react";
-import { useFormState } from "react-dom";
-import { toast } from "sonner";
-import SelectCategory  from "../components/SelectCategory";
-import { Textarea } from "@/components/ui/textarea";
-import { TipTapEditor } from "../components/Editor";
-import { UploadDropzone } from "@/app/lib/uploadthing";
-import { Button } from "@/components/ui/button";
+import { useEffect, useState } from 'react'
+import { useFormState } from 'react-dom'
+import { toast } from 'sonner'
+import SelectCategory from '../components/SelectCategory'
+import { Textarea } from '@/components/ui/textarea'
+import { TipTapEditor } from '../components/Editor'
+import { UploadDropzone } from '@/app/lib/uploadthing'
+import { Button } from '@/components/ui/button'
 // import { Submitbutton } from "../components/SubmitButtons";
 
-export function SellForm() {
-  const initalState: State = { message: "", status: undefined };
-  const [state, formAction] = useFormState(SellProduct, initalState);
-  const [json, setJson] = useState<null | JSONContent>(null);
-  const [images, setImages] = useState<null | string[]>(null);
-  const [productFile, SetProductFile] = useState<null | string>(null);
+export default function SellForm() {
+  const initalState: State = { message: '', status: undefined }
+  const [state, formAction] = useFormState(SellProduct, initalState)
+  const [json, setJson] = useState<null | JSONContent>(null)
+  const [images, setImages] = useState<null | string[]>(null)
+  const [productFile, SetProductFile] = useState<null | string>(null)
 
   useEffect(() => {
-    if (state.status === "success") {
-      toast.success(state.message);
-    } else if (state.status === "error") {
-      toast.error(state.message);
+    if (state.status === 'success') {
+      toast.success(state.message)
+    } else if (state.status === 'error') {
+      toast.error(state.message)
     }
-  }, [state]);
+  }, [state])
   return (
     <section className="max-w-7xl mx-auto px-4 md:px-8 mb-14">
       <Card>
@@ -57,16 +57,18 @@ export function SellForm() {
                 required
                 minLength={3}
               />
-              {state?.errors?.["name"]?.[0] && (
-                <p className="text-destructive">{state?.errors?.["name"]?.[0]}</p>
+              {state?.errors?.['name']?.[0] && (
+                <p className="text-destructive">
+                  {state?.errors?.['name']?.[0]}
+                </p>
               )}
             </div>
             <div className="flex flex-col gap-y-2">
               <Label>Category</Label>
               <SelectCategory />
-              {state?.errors?.["category"]?.[0] && (
+              {state?.errors?.['category']?.[0] && (
                 <p className="text-destructive">
-                  {state?.errors?.["category"]?.[0]}
+                  {state?.errors?.['category']?.[0]}
                 </p>
               )}
             </div>
@@ -80,8 +82,10 @@ export function SellForm() {
                 required
                 min={1}
               />
-              {state?.errors?.["price"]?.[0] && (
-                <p className="text-destructive">{state?.errors?.["price"]?.[0]}</p>
+              {state?.errors?.['price']?.[0] && (
+                <p className="text-destructive">
+                  {state?.errors?.['price']?.[0]}
+                </p>
               )}
             </div>
 
@@ -93,9 +97,9 @@ export function SellForm() {
                 required
                 minLength={10}
               />
-              {state?.errors?.["smallDescription"]?.[0] && (
+              {state?.errors?.['smallDescription']?.[0] && (
                 <p className="text-destructive">
-                  {state?.errors?.["smallDescription"]?.[0]}
+                  {state?.errors?.['smallDescription']?.[0]}
                 </p>
               )}
             </div>
@@ -108,56 +112,66 @@ export function SellForm() {
               />
               <Label>Description</Label>
               <TipTapEditor json={json} setJson={setJson} />
-              {state?.errors?.["description"]?.[0] && (
+              {state?.errors?.['description']?.[0] && (
                 <p className="text-destructive">
-                  {state?.errors?.["description"]?.[0]}
+                  {state?.errors?.['description']?.[0]}
                 </p>
               )}
             </div>
 
             <div className="flex flex-col gap-y-2">
-              <input type="hidden" name="images" value={JSON.stringify(images)} />
+              <input
+                type="hidden"
+                name="images"
+                value={JSON.stringify(images)}
+              />
               <Label>Product Images</Label>
               <UploadDropzone
                 endpoint="imageUploader"
-                onClientUploadComplete={(res) => {
-                  setImages(res.map((item) => item.url));
-                  toast.success("Your images have been uploaded");
+                onClientUploadComplete={res => {
+                  setImages(res.map(item => item.url))
+                  toast.success('Your images have been uploaded')
                 }}
                 onUploadError={(error: Error) => {
-                  toast.error("Something went wrong, try again");
+                  toast.error('Something went wrong, try again')
                 }}
               />
-              {state?.errors?.["images"]?.[0] && (
-                <p className="text-destructive">{state?.errors?.["images"]?.[0]}</p>
+              {state?.errors?.['images']?.[0] && (
+                <p className="text-destructive">
+                  {state?.errors?.['images']?.[0]}
+                </p>
               )}
             </div>
 
             <div className="flex flex-col gap-y-2">
-              <input type="hidden" name="productFile" value={productFile ?? ""} />
+              <input
+                type="hidden"
+                name="productFile"
+                value={productFile ?? ''}
+              />
               <Label>Product File</Label>
               <UploadDropzone
-                onClientUploadComplete={(res) => {
-                  SetProductFile(res[0].url);
-                  toast.success("Your Product file has been uplaoded!");
+                onClientUploadComplete={res => {
+                  SetProductFile(res[0].url)
+                  toast.success('Your Product file has been uplaoded!')
                 }}
                 endpoint="productFileUpload"
                 onUploadError={(error: Error) => {
-                  toast.error("Something went wrong, try again");
+                  toast.error('Something went wrong, try again')
                 }}
               />
-              {state?.errors?.["productFile"]?.[0] && (
+              {state?.errors?.['productFile']?.[0] && (
                 <p className="text-destructive">
-                  {state?.errors?.["productFile"]?.[0]}
+                  {state?.errors?.['productFile']?.[0]}
                 </p>
               )}
             </div>
           </CardContent>
           <CardFooter className="mt-5">
-                <Button type="submit">Submit Form</Button>
-              </CardFooter>
+            <Button type="submit">Submit Form</Button>
+          </CardFooter>
         </form>
       </Card>
     </section>
-  );
+  )
 }
