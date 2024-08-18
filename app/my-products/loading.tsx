f5dbfe1da64e97726d04c884b450c8b4d503847c
CodeMuscle <investportal99@gmail.com>
2024-08-18 10:49:38 +0530

    My Products Sell Route

--- app/my-products/loading.tsx
+++ app/my-products/loading.tsx
@@ -1,16 +1,18 @@
 import { Skeleton } from '@/components/ui/skeleton'
+import { LoadingProductCard } from '../components/ProductCard'
 
 export default function LoadingFile() {
   return (
     <section className="max-w-7xl mx-auto px-4 sm:px-8 mt-10">
-      <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-x-10">
-        <div className="col-span-1">
-          <Skeleton className="h-[250px] lg:h-[400px] w-full" />
-          <Skeleton className="h-[500px] mt-10 w-full" />
-        </div>
-        <div className="col-span-1">
-          <Skeleton className="h-[400px] w-full" />
-        </div>
+      <div className="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-col-3 mt-4 gap-10">
+        <LoadingProductCard />
+        <LoadingProductCard />
+        <LoadingProductCard />
+        <LoadingProductCard />
+        <LoadingProductCard />
+        <LoadingProductCard />
+        <LoadingProductCard />
+        <LoadingProductCard />
       </div>
     </section>
   )
