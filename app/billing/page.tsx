1e980b85ad4a0d347a27749cfabd12e77550a5b1
CodeMuscle <investportal99@gmail.com>
2024-08-19 15:51:04 +0530

    Updated dependencies

--- app/billing/page.tsx
+++ app/billing/page.tsx
@@ -1,13 +1,25 @@
-import { Card, CardHeader, CardTitle, CardDescription } from "@/components/ui/card"
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription
+} from '@/components/ui/card'
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 
-export default function BillingRoute() {
-  return(
+export default async function BillingRoute() {
+  const { getUser } = getKindeServerSession()
+
+  const user = await getUser()
+
+  if (!user) {
+    throw new Error('Unauthorized!')
+  }
+
+  return (
     <section className="max-w-7xl mx-auto px-4 md:px-8">
       <Card>
         <CardHeader>
-          <CardTitle>
-            Billing
-          </CardTitle>
+          <CardTitle>Billing</CardTitle>
           <CardDescription>
             Find all details regarding your payments
           </CardDescription>
